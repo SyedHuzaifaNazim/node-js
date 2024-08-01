e3f5584c3260578f5c7655862f3de7148d792ba2
SyedHuzaifaNazim <huzaifanazim154@gmail.com>
2024-08-01 20:46:11 +0500

    File System Modules

--- fsmodule.js
+++ fsmodule.js
@@ -8,4 +8,7 @@ console.log(a.toString())
 fs.writeFile('file2.txt', 'Its a data file ',()=>{
     console.log('file created')
 })
+
+const b = fs.writeFileSync('file2.txt', 'Its a data2 file ')
+console.log(b)
 console.log("Finished !!!!!!!!!!");
