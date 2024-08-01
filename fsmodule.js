1cbbb840e8d7ca6485ade6eda901cc52184df92f
SyedHuzaifaNazim <huzaifanazim154@gmail.com>
2024-08-01 20:28:07 +0500

    file system modules

---
+++ fsmodule.js
@@ -0,0 +1,11 @@
+const fs = require('node:fs');
+fs.readFile('file.txt', 'utf8', (err , data) => {
+    console.log(err,data)
+})
+const a = fs.readFileSync('file.txt')
+console.log(a.toString())
+
+fs.writeFile('file2.txt', 'Its a data file ',()=>{
+    console.log('file created')
+})
+console.log("Finished !!!!!!!!!!");
