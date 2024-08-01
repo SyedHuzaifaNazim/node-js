6225ebc86e88dcc11f75db5cf307aba268060cd3
SyedHuzaifaNazim <huzaifanazim154@gmail.com>
2024-08-01 21:14:16 +0500

    Common JS Modules VS ECMA Script Modules

---
+++ modulefirst.js
@@ -0,0 +1,11 @@
+// const simple = require("./modulesecond")
+// import { simple , simple2 } from "./modulesecond.mjs";
+// import simple22  from "./modulesecond.mjs";
+// import { simple2 as simple } from "./modulesecond.mjs";
+// import { simple as simple2 } from "./modulesecond.mjs";
+
+import * as a2 from "./modulesecond.mjs";
+
+console.log(a2.simple());
+// simple();
+// simple22();
---
+++ modulesecond.mjs
@@ -0,0 +1,11 @@
+export function simple(){
+    console.log("What people thinks its simple , that is more COMPLEX")
+    return 91
+}
+
+export default function simple2(){
+    console.log("JavaScript is that hard what people think it is..")
+}
+
+// module.exports = simple;
+
